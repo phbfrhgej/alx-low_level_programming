b6bcff53717468c53d63ebeb179b49601a831fd6
jo <joseph.byiringiro@flyzilpine.com>
2023-09-08 00:35:16 +0200

    code ofc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main entry of the program.
+*
+* return: 0 always
+*/
+int main(void)
+{
+/* my code have to be here */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
