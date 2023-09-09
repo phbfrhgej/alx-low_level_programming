8deb56912dda80ddc6b7a64e3c1f1a908ee090eb
jo <joseph.byiringiro@flyzilpine.com>
2023-09-09 20:45:10 +0200

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-print base 10 numbers
+ *
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\0');
+	return (0);
+}
