9763ed907ba4bdbf953147c1bbdc83a7142d2a6a
jo <joseph.byiringiro@flyzilpine.com>
2023-09-13 23:23:43 +0200

    100 time table

--- 0x02-functions_nested_loops/joseph.c
+++ 0x02-functions_nested_loops/joseph.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
@@ -6,7 +7,6 @@
 void print_times_table(int n)
 {
 int i, j;
-
 if (n < 0 || n > 15)
 return;
 for (i = 0; i <= n; i++)
@@ -22,11 +22,14 @@ _putchar(' ');
 }
 if (result < 10)
 _putchar(' ');
-if (result < 100)
 _putchar(' ');
+_putchar(result + '0');
 if (result >= 100)
-_putchar(result / 100 + '0');
+_putchar((result / 100) + '0');
+_putchar(((result / 10) % 10) + '0');
+_putchar((result % 10) + '0');
 if (result >= 10)
+_putchar(' ');
 _putchar((result / 10) % 10 + '0');
 _putchar(result % 10 + '0');
 }
